9f1a6593d1a1c872c86896a0a3dd9d7d89fce1f2
joseph.scott0605 <abC123,./>
2020-02-03 21:57:03 -0700

    Add project files.

---
+++ eXico.Shopify.Web.Core/Controllers/BaseControllers/Interfaces/IAppUnInstallController.cs
@@ -0,0 +1,26 @@
+﻿using Exico.Shopify.Data.Domain.AppModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Exico.Shopify.Web.Core.Controllers.BaseControllers.Interfaces
+{
+    public enum UNINSTALL_ACTIONS
+    {
+        AppUninstalled,
+        UnInstallCompleted,
+        UserIsRemoved,
+        CouldNotDeleteUser,
+        SendUninstallEmail
+    }
+
+    public interface IAppUnInstallController
+    {
+        Task<IActionResult> AppUninstalled(string userId);
+        Task<bool> SendUninstallEmail(AppUser user);
+        Task UnInstallCompleted(AppUser user);
+        Task UserIsDeleted(AppUser user);
+        Task CouldNotDeleteUser(AppUser user, Exception ex);
+    }
+}
---
+++ eXico.Shopify.Web.Core/Startup.cs
@@ -0,0 +1,54 @@
+﻿using Exico.Shopify.Web.Core.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Exico.Shopify.Web.Core
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            var mvcBuilder = services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
+            services.AddExicoShopifyRequiredServices(Configuration, mvcBuilder);  //adding services for our framework          
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseExicoShopifyFramework(env);
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.UseCookiePolicy();
+
+        }
+    }
+}
